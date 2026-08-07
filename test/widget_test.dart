39ef976a82d82b62498e3bd751bf03a5eb31f3be
linxh <960415449@qq.com>
2026-08-07 21:19:48 +0800

    Fix stable Flutter version and widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:hello_world_ios/main.dart';
 
 void main() {
-  testWidgets('显示 Hello World 页面', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(find.text('我的第一个应用'), findsOneWidget);
-    expect(find.text('Hello World'), findsOneWidget);
-    expect(find.text('应用特点'), findsOneWidget);
-
-    await tester.tap(find.text('开始探索'));
-    await tester.pump();
-    expect(find.text('已准备好'), findsOneWidget);
+  testWidgets('calculator page renders', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: CalculatorPage()));
+    expect(find.text('实时奖励计算'), findsOneWidget);
+    expect(find.text('蓝框均值'), findsOneWidget);
   });
 }
