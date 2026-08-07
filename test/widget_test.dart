24387de3646d047bf7b89815453a9b51c044ca08
linxh <960415449@qq.com>
2026-08-07 23:13:22 +0800

    Update widget test for calculator page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,11 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_world_ios/main.dart';
 
 void main() {
   testWidgets('calculator page renders', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: CalculatorPage()));
-    expect(find.text('实时奖励计算'), findsOneWidget);
-    expect(find.text('蓝框均值'), findsOneWidget);
+    expect(find.text('Reward Calculator'), findsOneWidget);
+    expect(find.textContaining('Average:'), findsOneWidget);
   });
 }
