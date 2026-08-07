020030213358e8cefd836f9141afcad38c9e1970
linxh <960415449@qq.com>
2026-08-07 23:30:43 +0800

    Align iOS delegates with Flutter 3.29

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,42 +1,13 @@
 import Flutter
 import UIKit
-import Vision
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-    let channel = FlutterMethodChannel(name: "com.secl.hello_world_ios/vision", binaryMessenger: engineBridge.binaryMessenger)
-    channel.setMethodCallHandler { call, result in
-      if call.method == "readLatestFrame" {
-        guard let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.secl.hello_world_ios") else { result(nil); return }
-        let url = container.appendingPathComponent("latest-frame.jpg")
-        result((try? Data(contentsOf: url)).map { FlutterStandardTypedData(bytes: $0) })
-        return
-      }
-      guard call.method == "recognizeDigits", let data = call.arguments as? FlutterStandardTypedData,
-            let image = UIImage(data: data.data), let cgImage = image.cgImage else {
-        result(FlutterMethodNotImplemented); return
-      }
-      let request = VNRecognizeTextRequest { request, error in
-        if let error { result(FlutterError(code: "OCR_ERROR", message: error.localizedDescription, details: nil)); return }
-        let values = (request.results as? [VNRecognizedTextObservation] ?? []).compactMap { $0.topCandidates(1).first?.string }
-        result(values)
-      }
-      request.recognitionLevel = .fast
-      request.usesLanguageCorrection = false
-      request.minimumTextHeight = 0.015
-      DispatchQueue.global(qos: .userInitiated).async {
-        do { try VNImageRequestHandler(cgImage: cgImage, options: [:]).perform([request]) }
-        catch { result(FlutterError(code: "OCR_ERROR", message: error.localizedDescription, details: nil)) }
-      }
-    }
   }
 }
